567afbb7bf8263e2578edc68e65f83d05091dfb8
michaelarmando <michaelarmandop@gmail.com>
2025-09-19 20:57:41 +0700

    1.1.6

--- api/data.php
+++ api/data.php
@@ -34,36 +34,78 @@ $portfolioItems = [
         'title' => 'Management KingsHCut',
         'category' => 'project',
         'image' => 'https://via.placeholder.com/400x250/112240/64ffda?text=KingsHCut',
-        'description' => 'Aplikasi web berbasis PHP dan MySQL untuk mengelola stok, penjualan, dan laporan keuangan barbershop.',
+        'description' => 'Aplikasi web berbasis java dan MySQL untuk mengelola stok, penjualan, dan laporan keuangan barbershop.',
         'link' => '#'
     ],
     [
         'title' => 'Aplikasi Sewa Alat Berat',
         'category' => 'project',
-        'image' => 'https://via.placeholder.com/400x250/112240/64ffda?text=Sewa+Alat',
-        'description' => 'Aplikasi untuk manajemen penyewaan alat berat yang efisien dan terintegrasi.',
-        'link' => '#'
+        'image' => 'https://cdn.momoi.cc/uploads/2025-09-19_1f6c11e925/2025-09-19_6b553724da.png',
+        'description' => 'Aplikasi sederhana untuk manajemen penyewaan alat berat yang efisien dan terintegrasi.',
+        'link' => 'https://www.figma.com/design/t3CNdPImrR6VpNo4JAhu6w/-2--ToolHub-Group-4-BD80?node-id=0-1&t=OnlmSoi4SiZW9Slu-1'
     ],
     [
         'title' => 'SMAK Penabur Harapan Indah',
         'category' => 'education',
         'image' => 'https://cdn.momoi.cc/uploads/2025-09-19_6895c42635/2025-09-19_47b7703c4d.jpg',
-        'description' => 'Lulus dari jurusan IPA di SMAK Penabur Harapan Indah.',
-        'link' => '#'
+        'description' => 'Lulus dari jurusan IPS di SMAK Penabur Harapan Indah.',
+        'link' => ''
     ],
     [
         'title' => 'BINUS University',
         'category' => 'education',
         'image' => 'https://cdn.momoi.cc/uploads/2025-09-19_c25637be7c/2025-09-19_06fe33cf1a.jpg',
         'description' => 'Saat ini menempuh pendidikan S1 di jurusan Business Information Technology.',
-        'link' => '#'
+        'link' => ''
     ],
     [
-        'title' => 'Sertifikat Kompetensi A',
+        'title' => 'Tutor Terbaik',
         'category' => 'certificate',
-        'image' => 'https://via.placeholder.com/400x250/112240/64ffda?text=Sertifikat',
-        'description' => 'Deskripsi singkat mengenai sertifikat yang diperoleh.',
-        'link' => '' // Kosongkan jika tidak ada link
+        'image' => 'https://cdn.momoi.cc/uploads/2025-09-19_d9070e03b1/2025-09-19_c9a72e5808.jpeg',
+        'description' => 'Diberikan sebagai penghargaan atas prestasi sebagai Tutor Terbaik selama tahun ajaran 2021/2022 di SMAK PENABUR Harapan Indah.',
+        'link' => '' 
+    ],
+    [
+        'title' => 'Character Growth',
+        'category' => 'certificate',
+        'image' => 'https://cdn.momoi.cc/uploads/2025-09-19_0ba68b1520/2025-09-19_9112458661.jpeg',
+        'description' => 'Penghargaan atas partisipasi dalam materi Spiritual, Kepercayaan, dan Etika pada tahun ajaran 2022/2023.',
+        'link' => '' 
+    ],
+    [
+        'title' => 'ENGLISH FOR CREATIVE WRITING',
+        'category' => 'certificate',
+        'image' => 'https://cdn.momoi.cc/uploads/2025-09-19_32fb2bca81/2025-09-19_f9fb7d070f.pdf',
+        'description' => 'Menyelesaikan program ENGLISH FOR CREATIVE WRITING Beelingua',
+        'link' => '' 
+    ],
+    [
+        'title' => 'ENGLISH FOR PROFESSIONAL OFFICE',
+        'category' => 'certificate',
+        'image' => 'https://cdn.momoi.cc/uploads/2025-09-19_272a689d3f/2025-09-19_7321c5bc4c.pdf',
+        'description' => 'Menyelesaikan program ENGLISH FOR PROFESSIONAL OFFICE Beelingua.',
+        'link' => '' 
+    ],
+    [
+        'title' => 'ADVANCED ENGLISH (CEFR C2.1)',
+        'category' => 'certificate',
+        'image' => 'https://cdn.momoi.cc/uploads/2025-09-19_acc28f7888/2025-09-19_69ba410bc2.pdf',
+        'description' => 'Menyelesaikan program ADVANCED ENGLISH (CEFR C2.1) Beelingua',
+        'link' => '' 
+    ],
+    [
+        'title' => 'MARKET RESEARCH & BUSINESS COMMUNICATION (CEFR C2.2)',
+        'category' => 'certificate',
+        'image' => 'https://cdn.momoi.cc/uploads/2025-09-19_63a262698c/2025-09-19_507cfdae9e.pdf',
+        'description' => 'Menyelesaikan program MARKET RESEARCH & BUSINESS COMMUNICATION (CEFR C2.2) Beelingua.',
+        'link' => '' 
+    ],
+    [
+        'title' => 'ENGLISH INDEPENDENT USER (CEFR B)',
+        'category' => 'certificate',
+        'image' => 'https://cdn.momoi.cc/uploads/2025-09-19_9ae92bc9d9/2025-09-19_d0ba5e5db5.pdf',
+        'description' => 'ENGLISH INDEPENDENT USER (CEFR B) Beelingua.',
+        'link' => '' 
     ],
 ];
 
